9e605ee2f967c4ea2b075229a06bc65a9262672b
Anup Mahato <anupmahato033@gmail.com>
2022-01-21 02:10:36 +0530

    'first-commit'

--- App.js
+++ App.js
@@ -1,17 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
+import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Provider } from 'react-redux';
+import HomeScreen from './screens/HomeScreen';
+import { store } from './store';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider store={store}>
+      <SafeAreaProvider>
+        <HomeScreen />
+      </SafeAreaProvider>
+
+    </Provider>
+
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    marginTop: 10,
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
---
+++ screens/HomeScreen.js
@@ -0,0 +1,38 @@
+import {
+    StyleSheet, Text, View,
+    SafeAreaView, Image
+} from 'react-native';
+import React from 'react';
+import tw from 'tailwind-react-native-classnames';
+import NavOptions from '../components/NavOptions';
+
+const HomeScreen = () => {
+    return (
+        <SafeAreaView style={tw`bg-white h-full`}>
+            <View style={tw`p-5`}>
+                <Image
+                    style={
+                        styles.uberLogo
+                    }
+                    source={{
+                        uri: "https:links.papareact.com/gzs"
+                    }
+                    } />
+            </View>
+            <NavOptions />
+        </SafeAreaView>
+    );
+};
+
+export default HomeScreen;
+
+const styles = StyleSheet.create({
+    text: {
+        color: 'blue',
+    },
+    uberLogo: {
+        width: 100,
+        height: 100,
+        resizeMode: "contain"
+    }
+});
